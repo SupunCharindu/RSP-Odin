36c63a840f711820f435ad19efe5fd3d72e5f79a
Supun <126235564+SupunCharindu@users.noreply.github.com>
2024-10-22 10:07:20 +0200

    I lost previous repo so i have to make it new but i add all steps here
     01 I take  random choice from computer i use if adn else i cot use array method
    
     02 Then i take input from user and make it lowercase letter
    
     03 I make one round function in that one round i make how player can win and add one score for player, other way if i player lose
        computer had one score
    
     04 And then I make function for the game. game will play 5 rounds so i make loop the round function for 5 time and most round winner is the game winner
    
     i use console to show each round results

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RSP-Odin
 RSP game
+
+This Game can play buy two one is computer 
+computer choose randomly paper rock or scissors,And player can choose his choice,
+then we make it up to five rounds most round winner will be the winner and game END.
---
+++ script.js
@@ -0,0 +1,89 @@
+
+
+//computer choice
+function getComputerChoice(){
+  let choice = Math.floor(Math.random() * 3);
+
+  if(choice === 0){
+    return 'rock'
+  }else if (choice === 1){
+    return 'paper'
+  }else{
+    return 'scissors'
+  }
+}
+
+//console.log(getComputerChoice())
+
+//Get Player choice
+function getHumanChoice(){
+  let choices = ['rock' , 'paper' ,'scissors' ]
+  let choice = prompt('Please choose one, rock paper or scissors').toLowerCase();
+  
+  while(!choices.includes(choice)){
+    choice =  prompt('INVALID iNPUT! Please choose one, rock paper or scissors').toLowerCase()
+  };
+    return choice;
+  
+}
+
+//console.log(getHumanChoice());
+
+// score
+let humanScore = 0;
+let computerScore = 0;
+
+//game round
+function playRound(humanChoice, computerChoice){
+  humanChoice = humanChoice.toLowerCase();
+  
+
+  if(humanChoice === computerChoice){
+    console.log("It' a Tie");
+    }else if(humanChoice === 'rock' && computerChoice === 'scissors' ||
+            humanChoice === 'paper' && computerChoice === 'rock' ||
+            humanChoice == 'scissors' && computerChoice === 'paper'){
+              humanScore++;
+              console.log("YOU WIN, your choice " + humanChoice + " beats " + computerChoice + " and you win one point");            
+    }else{
+      computerScore++;
+      console.log("YOU LOSE, your choice " + computerChoice + " beats " + humanChoice + " and computer win one point");      
+    }
+    
+}
+
+
+
+//play game for 5 rounds
+function playGame(){
+  
+
+  for (let i = 0; i < 5; i++){
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log('-------------------')
+    
+  }
+  console.log('game over')
+  
+  if(humanScore > computerScore){
+    console.log(prompt("YOU WON THE GAME!"))
+  }else if(humanScore < computerScore){
+    console.log(prompt("YOU LOSE THE GAME!"))
+  }else{
+    console.log(prompt("IT's a tie!"))
+  }
+ 
+  
+}
+
+playGame();
+
+
+
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+// playRound(humanSelection, computerSelection);
